78864091a120fdbe17b062066ee4f66aa9dc814e
Elsis-Sitati <sitatielsis@gmail.com>
2016-04-15 14:18:02 +0300

    added smooth scroller jquerty

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -25,3 +25,17 @@ $.fn.typewriter = function () {
     return this
 };
 $(".terminal").typewriter();
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
